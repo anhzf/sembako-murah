7e30f1017800a849cb2283cb2ee3aecb811f6ab5
Anhzf <36941633+anhzf@users.noreply.github.com>
2021-06-19 18:39:01 +0700

    feat(page): :sparkles: login: auth backend integration

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -1,41 +1,45 @@
 @extends('layouts.auth')
 
 @section('title')
-Login
+  Login
 @endsection
 
 @section('content')
-<!-- Page Content -->
-<div class="page-content page-auth">
-  <section class="section-store-auth" data-aos="fade-up">
-    <div class="container">
-      <div class="row align-items-center row-login">
-        <div class="col-lg-6 text-center">
-          <img src="/images/login-placeholder.png" alt="" class="w-50 mb-4 mb-lg-none" />
-        </div>
-        <div class="col-lg-5">
-          <h2>Belanja kebutuhan utama ada, <br />Menjadi lebih mudah</h2>
-          <form class="mt-3">
-            <div class="mb-3">
-              <label for="email" class="form-label mb-1">Email address</label>
-              <input type="email" class="form-control w-75" id="email" aria-describedby="emailHelp" />
-            </div>
-            <div class="mb-3">
-              <label for="password" class="form-label mb-1">Password</label>
-              <input type="password" class="form-control w-75" id="password" />
-            </div>
-            <a href="/dashboard.html" class="btn btn-success d-grid gap-2 w-75 mt-4">
-              Sign In to My Account
-            </a>
-            <a href="/register.html" class="btn btn-signup d-grid w-75 mt-2">
-              Sign Up
-            </a>
-          </form>
+  <!-- Page Content -->
+  <div class="page-content page-auth">
+    <section class="section-store-auth" data-aos="fade-up">
+      <div class="container">
+        <div class="row align-items-center row-login">
+          <div class="col-lg-6 text-center">
+            <img src="/images/login-placeholder.png" alt="" class="w-50 mb-4 mb-lg-none" />
+          </div>
+
+          <div class="col-lg-5">
+            <h2>Belanja kebutuhan utama ada, <br />Menjadi lebih mudah</h2>
+
+            <form method="POST" action="{{ route('login') }}" class="mt-3">
+              @csrf
+
+              <x-input label="Email address" type="email" name="email" class="mb-3" input-class="w-75" required
+                autofocus />
+
+              <x-input label="Password" type="password" name="password" class="mb-3" input-class="w-75" required />
+
+              <x-checkbox :label="__('Remember me')" name="remember" />
+
+              <button type="submit" class="btn btn-success d-grid gap-2 w-75 mt-4">
+                Sign In to My Account
+              </button>
+
+              <a href="{{ route('register') }}" class="btn btn-signup d-grid w-75 mt-2">
+                Sign Up
+              </a>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
-</div>
+    </section>
+  </div>
 @endsection
 
 @push('add-script')
