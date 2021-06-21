11e6dc0f3af5f08e5df647b18e49e2571e18c01c
fahmih678 <fahmih678@gmail.com>
2021-06-21 22:44:49 +0700

    update logo, navbar, sidebar, about

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -67,9 +67,11 @@
     <div class="container">
       <div class="row justify-content-md-center">
         <div class="col-md-auto" data-aos="fade-up">
-          <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi molestias sequi hic
-            officiis quos ex eum
-            fugit? Doloremque, commodi. Illo harum id provident ad nemo blanditiis mollitia deserunt fugiat repellat.
+          <p class="text-center">Website ini merupakan sebuah website yang melayani penjualan berbagai macam jenis dam
+            merek sembako yang dibutuhkan oleh masyarakat setiap harinya. penjualan dilakukan dengan cara dalam jaringan
+            mengingat penyebaran virus korona yang belum juga untuk berhenti. dengan adanya website ini diharapkan dapat
+            membantu masyarakat untuk memenuhi kebutuhan sembakonya dengan tidak melanggar protokol kesehatan yang
+            berlaku.
           </p>
         </div>
       </div>
@@ -105,28 +107,35 @@
               <div class="profile_data">
                 <table style="width:100%">
                   <tr>
-                    <th>Age</th>
+                    <th>NIM</th>
                     <th>:</th>
-                    <td>20 year old</td>
+                    <td>K3519010</td>
                   </tr>
                   <tr>
-                    <th>Location</th>
+                    <th>Kelas</th>
                     <th>:</th>
-                    <td>Java, Indonesia</td>
+                    <td>B</td>
                   </tr>
                   <tr>
-                    <th>Experience</th>
+                    <th>Prodi</th>
                     <th>:</th>
-                    <td>2 year</td>
+                    <td>PTIK 2019</td>
                   </tr>
                   <tr>
                     <th>Carrer Level</th>
                     <th>:</th>
-                    <td>Beginner</td>
+                    <td>Pro Player</td>
                   </tr>
                 </table>
               </div>
-              <a href="#" class="btn btn-dark">Go somewhere</a>
+              <div class="mt-2">
+                <a href="{{ url('https://github.com/fahmih678') }}" class=""><button
+                    class="btn btn-dark align-middle"><svg class="" xmlns="http://www.w3.org/2000/svg" width="16"
+                      height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                      <path
+                        d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                    </svg> anhzf</button></a>
+              </div>
             </div>
           </div>
         </div>
@@ -143,28 +152,36 @@
                 <table style="width:100%">
 
                   <tr>
-                    <th>Age</th>
+                    <th>NIM</th>
                     <th>:</th>
-                    <td>20 year old</td>
+                    <td>K3519070</td>
                   </tr>
                   <tr>
-                    <th>Location</th>
+                    <th>Kelas</th>
                     <th>:</th>
-                    <td>Java, Indonesia</td>
+                    <td>B</td>
                   </tr>
                   <tr>
-                    <th>Experience</th>
+                    <th>Prodi</th>
                     <th>:</th>
-                    <td>2 year</td>
+                    <td>PTIK 2019</td>
                   </tr>
                   <tr>
                     <th>Carrer Level</th>
                     <th>:</th>
-                    <td>Beginner</td>
+                    <td>Begginer</td>
                   </tr>
                 </table>
               </div>
-              <a href="#" class="btn btn-dark">Go somewhere</a>
+              <div class="mt-2">
+
+                <a href="{{ url('https://github.com/fahmih678') }}" class=""><button
+                    class="btn btn-dark align-middle"><svg class="" xmlns="http://www.w3.org/2000/svg" width="16"
+                      height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                      <path
+                        d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                    </svg> fahmih678</button></a>
+              </div>
             </div>
           </div>
         </div>
@@ -181,28 +198,36 @@
                 <table style="width:100%">
 
                   <tr>
-                    <th>Age</th>
+                    <th>NIM</th>
                     <th>:</th>
-                    <td>20 year old</td>
+                    <td>K3519035</td>
                   </tr>
                   <tr>
-                    <th>Location</th>
+                    <th>Kelas</th>
                     <th>:</th>
-                    <td>Java, Indonesia</td>
+                    <td>A</td>
                   </tr>
                   <tr>
-                    <th>Experience</th>
+                    <th>Prodi</th>
                     <th>:</th>
-                    <td>2 year</td>
+                    <td>PTIK 2019</td>
                   </tr>
                   <tr>
                     <th>Carrer Level</th>
                     <th>:</th>
-                    <td>Beginner</td>
+                    <td>Pro Player</td>
                   </tr>
                 </table>
               </div>
-              <a href="#" class="btn btn-dark">Go somewhere</a>
+              <div class="mt-2">
+
+                <a href="{{ url('https://github.com/hamidy') }}" class=""><button class="btn btn-dark align-middle"><svg
+                      class="" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                      class="bi bi-github" viewBox="0 0 16 16">
+                      <path
+                        d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                    </svg> hamidy</button></a>
+              </div>
             </div>
           </div>
         </div>
@@ -216,28 +241,37 @@
               <div class="profile_data">
                 <table style="width:100%">
                   <tr>
-                    <th>Age</th>
+                    <th>NIM</th>
                     <th>:</th>
-                    <td>20 year old</td>
+                    <td>K3519009</td>
                   </tr>
                   <tr>
-                    <th>Location</th>
+                    <th>Kelas</th>
                     <th>:</th>
-                    <td>Java, Indonesia</td>
+                    <td>A</td>
                   </tr>
                   <tr>
-                    <th>Experience</th>
+                    <th>Prodi</th>
                     <th>:</th>
-                    <td>2 year</td>
+                    <td>PTIK 2019</td>
                   </tr>
                   <tr>
                     <th>Carrer Level</th>
                     <th>:</th>
-                    <td>Beginner</td>
+                    <td>Pro Player</td>
                   </tr>
                 </table>
               </div>
-              <a href="#" class="btn btn-dark">Go somewhere</a>
+              <div class="mt-2">
+
+                <a href="{{ url('https://github.com/alvink') }}" class=""><button class="btn btn-dark align-middle"><svg
+                      class="" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                      class="bi bi-github" viewBox="0 0 16 16">
+                      <path
+                        d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                    </svg> alvink</button></a>
+                {{-- <a href="" class="btn btn-light">Go</a> --}}
+              </div>
             </div>
           </div>
         </div>
@@ -247,7 +281,7 @@
   </section>
 
   <!-- Footer -->
-  <section class="footer section">
+  {{-- <section class="footer section">
     <div class="container">
       <div class="row">
         <div class="col-lg-4 col-sm-12">
@@ -277,6 +311,6 @@
         </div>
       </div>
 
-  </section>
+  </section> --}}
 </div>
 @endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,121 +1,121 @@
-@inject('formatter', '\App\Helpers\Formatter');
+@inject('formatter', '\App\Helpers\Formatter')
 
 @extends('layouts.front')
 
 @section('content')
-  {{-- Page Content --}}
-  <div class="page-content page-home">
-    <!-- Home Carousel -->
-    <section class="store-carousel">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12" data-aos="zoom-in">
-            <div id="homeCarousel" class="carousel slide carousel-dark" data-bs-ride="carousel">
-              <div class="carousel-indicators">
-                <button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="0" class="active"
-                  aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
-              </div>
-              <div class="carousel-inner">
-                <div class="carousel-item active">
-                  <img src="/images/banner.jpg" class="d-block w-100" alt="Carousel Image 1" />
-                  <div class="carousel-caption d-none d-md-block">
-                    <h5>First slide label</h5>
-                    <p>
-                      Some representative placeholder content for the first
-                      slide.
-                    </p>
-                  </div>
-                </div>
-                <div class="carousel-item">
-                  <img src="/images/banner.jpg" class="d-block w-100" alt="Carousel Image 2" />
-                  <div class="carousel-caption d-none d-md-block">
-                    <h5>Second slide label</h5>
-                    <p>
-                      Some representative placeholder content for the second
-                      slide.
-                    </p>
-                  </div>
-                </div>
-                <div class="carousel-item">
-                  <img src="/images/banner.jpg" class="d-block w-100" alt="Carousel Image 3" />
-                  <div class="carousel-caption d-none d-md-block">
-                    <h5>Third slide label</h5>
-                    <p>
-                      Some representative placeholder content for the third
-                      slide.
-                    </p>
-                  </div>
+{{-- Page Content --}}
+<div class="page-content page-home">
+  <!-- Home Carousel -->
+  <section class="store-carousel">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12" data-aos="zoom-in">
+          <div id="homeCarousel" class="carousel slide carousel-dark" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+              <button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="0" class="active"
+                aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#homeCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="/images/banner.jpg" class="d-block w-100" alt="Carousel Image 1" />
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>First slide label</h5>
+                  <p>
+                    Some representative placeholder content for the first
+                    slide.
+                  </p>
                 </div>
               </div>
-              <button class="carousel-control-prev" type="button" data-bs-target="#homeCarousel" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-              </button>
-              <button class="carousel-control-next" type="button" data-bs-target="#homeCarousel" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- Carousel -->
-    </section>
-
-    <!-- Categories -->
-    <section class="store-trend-categories">
-      <div class="container">
-        <div class="row">
-          <div class="col-12" data-aos="fade-up">
-            <h5>Featured Categories</h5>
-          </div>
-        </div>
-        <div class="row">
-          <!-- Component Categories -->
-          @foreach ($categoryList as $category)
-            <div class="col-6 col-md-3 col-lg-2" data-aos="fade-up" data-aos-delay="100">
-              <a href="{{ route('home', ['category' => $category->slug]) }}" class="component-categories d-block">
-                <div class="categories-image">
-                  <img src="/images/categories-herbal.svg" alt="" class="w-100" />
+              <div class="carousel-item">
+                <img src="/images/banner.jpg" class="d-block w-100" alt="Carousel Image 2" />
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Second slide label</h5>
+                  <p>
+                    Some representative placeholder content for the second
+                    slide.
+                  </p>
                 </div>
-                <p class="categories-text">{{ $category->name }}</p>
-              </a>
+              </div>
+              <div class="carousel-item">
+                <img src="/images/banner.jpg" class="d-block w-100" alt="Carousel Image 3" />
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Third slide label</h5>
+                  <p>
+                    Some representative placeholder content for the third
+                    slide.
+                  </p>
+                </div>
+              </div>
             </div>
-          @endforeach
-        </div>
-      </div>
-    </section>
-
-    <!-- New Product -->
-    <section class="store-new-product">
-      <div class="container">
-        <div class="row">
-          <div class="col-12" data-aos="fade-up">
-            <h5>Featured Products</h5>
+            <button class="carousel-control-prev" type="button" data-bs-target="#homeCarousel" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#homeCarousel" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
           </div>
         </div>
-        <div class="row">
-          <!-- Component New Product -->
-          @foreach ($productList as $product)
-            <div class="col-6 col-md-4 col-lg-3" data-aos="fade-up" data-aos-delay="100">
-              <a href="{{ route('detail', $product->getKey()) }}" class="component-products d-block">
-                <div class="products-thumbnail">
-                  @if (isset($product->photos[0]))
-                    <img src="{{ $product->photos[0] }}" alt="" class="products-image">
-                  @else
-                    <img src="https://via.placeholder.com/200?{{ Arr::query(['text' => 'No image']) }}" alt=""
-                      class="products-image">
-                  @endif
-                </div>
-                <div class="products-text">{{ $product->name }}</div>
-                <div class="products-price">{{ $formatter::intToIdr($product->price) }}</div>
-              </a>
-            </div>
-          @endforeach
+      </div>
+    </div>
+    <!-- Carousel -->
+  </section>
+
+  <!-- Categories -->
+  <section class="store-trend-categories">
+    <div class="container">
+      <div class="row">
+        <div class="col-12" data-aos="fade-up">
+          <h5>Featured Categories</h5>
         </div>
       </div>
-    </section>
-  </div>
+      <div class="row">
+        <!-- Component Categories -->
+        @foreach ($categoryList as $category)
+        <div class="col-6 col-md-3 col-lg-2" data-aos="fade-up" data-aos-delay="100">
+          <a href="{{ route('home', ['category' => $category->slug]) }}" class="component-categories d-block">
+            <div class="categories-image">
+              <img src="/images/categories-herbal.svg" alt="" class="w-100" />
+            </div>
+            <p class="categories-text">{{ $category->name }}</p>
+          </a>
+        </div>
+        @endforeach
+      </div>
+    </div>
+  </section>
+
+  <!-- New Product -->
+  <section class="store-new-product">
+    <div class="container">
+      <div class="row">
+        <div class="col-12" data-aos="fade-up">
+          <h5>Featured Products</h5>
+        </div>
+      </div>
+      <div class="row">
+        <!-- Component New Product -->
+        @foreach ($productList as $product)
+        <div class="col-6 col-md-4 col-lg-3" data-aos="fade-up" data-aos-delay="100">
+          <a href="{{ route('detail', $product->getKey()) }}" class="component-products d-block">
+            <div class="products-thumbnail">
+              @if (isset($product->photos[0]))
+              <img src="{{ $product->photos[0] }}" alt="" class="products-image">
+              @else
+              <img src="https://via.placeholder.com/200?{{ Arr::query(['text' => 'No image']) }}" alt=""
+                class="products-image">
+              @endif
+            </div>
+            <div class="products-text">{{ $product->name }}</div>
+            <div class="products-price">{{ $formatter::intToIdr($product->price) }}</div>
+          </a>
+        </div>
+        @endforeach
+      </div>
+    </div>
+  </section>
+</div>
 @endsection
