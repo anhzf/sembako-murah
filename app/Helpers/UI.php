c64a9583eff3d480ce391e775d62254f3098bbec
Anhzf <36941633+anhzf@users.noreply.github.com>
2021-06-22 15:07:49 +0700

    fix(ui): :lipstick: fix UI::notification wrong classname

--- app/Helpers/UI.php
+++ app/Helpers/UI.php
@@ -6,7 +6,7 @@ class UI
 {
   const NOTIFY_SESSNAME = 'notif';
   const NOTIFY_SUCCESS_SESSNAME = 'success';
-  const NOTIFY_ERROR_SESSNAME = 'error';
+  const NOTIFY_ERROR_SESSNAME = 'danger';
   const NOTIFY_INFO_SESSNAME = 'info';
   const NOTIFY_WARNING_SESSNAME = 'warning';
   const NOTIFY_PRIMARY_SESSNAME = 'primary';
@@ -36,7 +36,7 @@ class UI
   public static function notifyError(string ...$messages)
   {
     collect(...$messages)
-      ->each(fn ($msg) => UI::notify(UI::NOTIFY_SUCCESS_SESSNAME, $msg));
+      ->each(fn ($msg) => UI::notify(UI::NOTIFY_ERROR_SESSNAME, $msg));
   }
 
   public static function notifyInfo(string ...$messages)
